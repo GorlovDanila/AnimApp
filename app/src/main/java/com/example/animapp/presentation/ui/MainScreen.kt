fe88fa110eca0e14e8f2332a447870271b926b42
GorlovDanila <danildanil20030@gmail.com>
2023-05-23 18:04:29 +0300

    delete unused import

--- app/src/main/java/com/example/animapp/presentation/ui/MainScreen.kt
+++ app/src/main/java/com/example/animapp/presentation/ui/MainScreen.kt
@@ -1,17 +1,9 @@
 package com.example.animapp.presentation.ui
 
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
